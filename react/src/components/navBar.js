c6d6b753c7fd6abd33762e38bd762b7d7a550b54
Liz Chhouk <kisaras.playground@gmail.com>
2017-05-05 13:01:20 -0400

    removed styling from StickyContainer

--- react/src/components/navBar.js
+++ react/src/components/navBar.js
@@ -12,33 +12,31 @@ class NavBar extends Component {
     return(
       <StickyContainer>
         <Sticky>
-          {
-            ({ isSticky, wasSticky, style, distanceFromTop, distanceFromBottom, calculatedHeight }) => {
-              return (
-                <div className="row" id="nav-bar">
-                  <div className="small-12 large-12 columns" id="nav-bar">
-                    <div className="menu">
-                      <ul className="menu align-left">
-                        <li id="logo">
-                          <a href = "/">
-                            <span style={{font: "400 4.0em/100% 'Italianno', Helvetica, Sans-serif"}}>
-                              Kaiwa
-                            </span>
-                          </a>
-                        </li>
-                      </ul>
-                      <NavBarRight
-                        query = {this.props.query}
-                        onSearchQuery = {this.props.onSearchQuery}
-                        handleSearchButton = {this.props.handleSearchButton}
-                        current_user = {this.props.current_user}
-                      />
-                    </div>
+          { () => {
+            return (
+              <div className="row" id="nav-bar">
+                <div className="small-12 large-12 columns" id="nav-bar">
+                  <div className="menu">
+                    <ul className="menu align-left">
+                      <li id="logo">
+                        <a href = "/">
+                          <span style={{font: "400 4.0em/100% 'Italianno', Helvetica, Sans-serif"}}>
+                            Kaiwa
+                          </span>
+                        </a>
+                      </li>
+                    </ul>
+                    <NavBarRight
+                      query = {this.props.query}
+                      onSearchQuery = {this.props.onSearchQuery}
+                      handleSearchButton = {this.props.handleSearchButton}
+                      current_user = {this.props.current_user}
+                    />
                   </div>
                 </div>
-              )
-            }
-          }
+              </div>
+            )
+          } }
         </Sticky>
       </StickyContainer>
     )
