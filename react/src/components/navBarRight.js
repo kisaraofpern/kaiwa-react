fd58df311695ec9c9f07f8bac88f6c1783e4bf2a
Liz Chhouk <kisaras.playground@gmail.com>
2017-05-05 12:41:53 -0400

    renamed searchBar.js

--- react/src/components/navBarRight.js
+++ react/src/components/navBarRight.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router';
-import SearchBar from './SearchBar';
+import SearchBar from './searchBar';
 
 class NavBarRight extends Component {
   constructor (props) {
