7d85e463445ff0fa3009b20102682626278740ee
Liz Chhouk <kisara.of.pern@gmail.com>
2017-05-05 23:10:41 -0400

    Resolved Hound violation #1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :authorize_user, only: [:edit, :update, :show]
 
-  def show;end
+  def show; end
 
-  def edit;end
+  def edit; end
 
-  def update;end
+  def update; end
 end
